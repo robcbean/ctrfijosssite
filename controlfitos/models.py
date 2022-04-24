466bde6e5acc87aecf7bac1650e4ab3a5ff182d5
Roberto Beán <robcbean@gmail.com>
2022-04-24 08:45:33 +0200

    Ajustes bases de datos

--- controlfitos/models.py
+++ controlfitos/models.py
@@ -1,70 +1,75 @@
-from django.db import models
 
+
+import django
 from django.db import models
 
 
 class Cliente(models.Model):
-    nombre = models.CharField(max_length=100)
-    domicilio = models.CharField(max_length=250)
-    cif = models.CharField(max_length=10)
+    nombre = models.CharField(max_length=100,default='')
+    domicilio = models.CharField(max_length=250,default='')
+    cif = models.CharField(max_length=10,default='')
 
 class ConceptoGasto(models.Model):
-    comentario = models.CharField(max_length=100)
+    comentario = models.CharField(max_length=100,default='')
 
 class Cultivo(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100,default='')
 
 class Variedad(models.Model):
-    nombre = models.CharField(max_length=100)
-    finalizada = models.IntegerField()
+    nombre = models.CharField(max_length=100,default='')
+    finalizada = models.IntegerField(default=0)
     cultivo = models.ForeignKey(Cultivo,on_delete=models.CASCADE)
 
 class ConceptoGasto(models.Model):
-    comentrio = models.CharField(max_length=100)
+    comentrio = models.CharField(max_length=100,default='')
 
 
 class TipoTratamiento(models.Model):
-    nombre = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100,default='')
 
 
 class Gasto(models.Model):
-    fecha = models.DateField()
+    fecha = models.DateField(default=django.utils.timezone.now)
     cultivo = models.ForeignKey(Cultivo,on_delete=models.CASCADE)
     variedad = models.ForeignKey(Variedad,on_delete=models.CASCADE)
     concepto = models.ForeignKey(ConceptoGasto,on_delete=models.CASCADE)
 
 
 class Agricultor(models.Model):
-    nombre = models.CharField(max_length=100)
-    campanya = models.CharField(max_length=100)
-    domicilio = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=100,default='')
+    campanya = models.CharField(max_length=100,default='')
+    domicilio = models.CharField(max_length=250,default='')
     cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
-    cif = models.CharField(max_length=10)
+    cif = models.CharField(max_length=10,default='')
 
 class CabSalida(models.Model):
-    fecha = models.DateField()
+    fecha = models.DateField(default=django.utils.timezone.now)
 
 class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    nombreComercial = models.CharField(max_length=100)
-    noregistro = models.CharField(max_length=100)
-    precio = models.FloatField()
-    plazoSeguridad = models.IntegerField()
+    nombre = models.CharField(max_length=100,default='')
+    nombreComercial = models.CharField(max_length=100,default='')
+    noregistro = models.CharField(max_length=100,default='')
+    precio = models.FloatField(default=0)
+    plazoSeguridad = models.IntegerField(default=0)
     tipoTratamiento = models.ForeignKey(TipoTratamiento,on_delete=models.CASCADE)
 
 class Salida(models.Model):
-    cantidad = models.FloatField()
-    precio = models.FloatField()
-    albaran = models.CharField(max_length=100)
-    factura = models.CharField(max_length=100)
-    cabSalida = models.ForeignKey(CabSalida,on_delete=models.CASCADE)
-    cliente = models.ForeignKey(Cliente,on_delete=models.CASCADE)
-
+    cantidad = models.FloatField(default=0)
+    precio = models.FloatField(default=0)
+    albaran = models.CharField(max_length=100,default='')
+    factura = models.CharField(max_length=100,default='')
+    cabSalida = models.ForeignKey(CabSalida, on_delete=models.CASCADE)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    variedad = models.ForeignKey(Variedad, on_delete=models.CASCADE)
 
 class Tratamiento(models.Model):
-    fecha = models.DateField()
-    precio = models.FloatField()
+    fecha = django.utils.timezone.now
+    precio = models.FloatField(default=0)
+    producto = models.ForeignKey(Producto,on_delete=models.CASCADE)
 
+class VariedadesTratamiento(models.Model):
+    tratamiento = models.ForeignKey(Tratamiento,on_delete=models.CASCADE)
+    variedad = models.ForeignKey(Variedad,on_delete=models.CASCADE)
 
 
 
